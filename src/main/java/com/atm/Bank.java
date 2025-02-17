021be447e535b6a6e66720440317e4a26d8e7766
unknown <mck0391@gmail.com>
2025-02-17 11:43:48 +0000

    Task 2 Mertcan

--- src/main/java/com/atm/Bank.java
+++ src/main/java/com/atm/Bank.java
@@ -1,10 +1,6 @@
 package com.atm;
 
-// Mertcan Kara
-//11 Feb 13.27
-//litaaaaaaa
-//Nadeen Samad Test
-//bora
+// Mertcan Kara Task 2 Update
 
 // Bank class - simple implementation of a bank, with a list of bank accounts, an
 // a current account that we are logged in to.
