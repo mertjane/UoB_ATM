11db781a6045416a0644c91d4a039969985aa1c8
unknown <mck0391@gmail.com>
2025-03-25 13:17:47 +0000

    @Mertcan week 7: Prompt users to check balance before withdrawing money || Integrate sound effects for user actions like button clicks. || Implemented DatabaseConnection SQL for future.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.atm {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql; // declaring the dependency on java.sql module
+    requires javafx.media; // Added for AudioClip
 
     opens com.atm to javafx.fxml;
     exports com.atm;
